f801267b8b26028b5cd7c7598791a541745ffea5
Trojan9 <timmybanjo@gmail.com>
2020-07-22 13:10:32 +0100

    Updates

---
+++ precache-manifest.20e22a5d6dc1197992a650c08ef3affa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f107d57f9b65d64f021ed0e1bcb8334",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "99e04f2f7e8977f4a40f",
+    "url": "/todoapp/static/css/main.1fca340e.chunk.css"
+  },
+  {
+    "revision": "1adba280f785b65c5ede",
+    "url": "/todoapp/static/js/2.36b1da36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.36b1da36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e04f2f7e8977f4a40f",
+    "url": "/todoapp/static/js/main.b65346df.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
